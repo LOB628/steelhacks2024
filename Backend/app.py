eb146ecab1331c990a87aee5352b3a641da83663
ColeMinersUnion <colehansen018@gmail.com>
2024-09-28 12:38:36 -0400

    Made an api endpoint to call gemini.

--- Backend/app.py
+++ Backend/app.py
@@ -1,6 +1,22 @@
-from flask import Flask
+#API Stuff
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
+#Utils
 from dotenv import load_dotenv
+#Gemini Functions
+from Gemini.Chat import LiveChat
+from Gemini.Gemini import Gemini as Gemi
 
 app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
+cors = CORS(app, resources={r"/api": {"origins":"*"}})
 
+chatObj = LiveChat
 
+@app.route('/api/v1/gemini', methods=['POST'])
+@cross_origin(origin="*", headers=['Content-Type', 'Authorization'])
+def Gemini():
+    prompt = request.json['Prompt']
+    #print(prompt['content'])
+    print(Gemi(prompt['content']))
+    return 'Success', 200
