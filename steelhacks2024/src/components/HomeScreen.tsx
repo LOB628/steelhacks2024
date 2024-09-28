8d68667c22cc1f1cebc06820bd36bfb5a72c5166
Zach Schultz <zachjacob7@gmail.com>
2024-09-28 15:56:52 -0400

    Frontend

---
+++ steelhacks2024/src/components/HomeScreen.tsx
@@ -0,0 +1,129 @@
+import React from 'react'
+import Dropdown from 'react-bootstrap/Dropdown'
+
+function HomeScreen() {
+    const majors = [
+        "Accounting",
+        "Actuarial Mathematics",
+        "Africana Studies",
+        "Africana Studies–English",
+        "Anthropology",
+        "Applied Developmental Psychology",
+        "Applied Mathematics",
+        "Architecture",
+        "Architectural Studies",
+        "Astronomy",
+        "Biochemistry",
+        "Bioengineering",
+        "Biological Sciences",
+        "Business Information Systems",
+        "CASE Teacher Preparation",
+        "Chemical Engineering",
+        "Chemistry",
+        "Chinese",
+        "Civil Engineering",
+        "Classics",
+        "Communication: Rhet & Comm",
+        "Communication Science",
+        "Computational Biology",
+        "Computer Engineering",
+        "Computer Science",
+        "Computational Social Science",
+        "Data Science",
+        "Dental Hygiene",
+        "Dietician Nutritionist Program",
+        "Digital Narrative and Interactive Design",
+        "Ecology and Evolution",
+        "Economics",
+        "Economics–Statistics",
+        "Electrical Engineering",
+        "Emergency Medicine",
+        "Engineering Science",
+        "English Literature",
+        "English Writing",
+        "Environmental Science",
+        "Environmental Studies",
+        "Exercise Science",
+        "Film and Media Studies",
+        "Finance",
+        "French",
+        "Gender, Sexuality, and Women's Studies",
+        "Geology",
+        "German",
+        "Global Management",
+        "Health Informatics",
+        "Health Services",
+        "Health and Physical Activity",
+        "History",
+        "History and Philosophy of Science",
+        "History of Art & Architecture",
+        "Human Resources Management",
+        "Industrial Engineering",
+        "Information Science",
+        "International and Area Studies",
+        "International Studies Co-Major",
+        "Italian",
+        "Italian Studies",
+        "Japanese",
+        "Law, Criminal Justice, and Society",
+        "Linguistics",
+        "Marketing",
+        "Materials Science and Engineering",
+        "Mathematics",
+        "Mathematical Biology",
+        "Mathematics-Economics",
+        "Mechanical Engineering",
+        "Media and Professional Communications",
+        "Microbiology",
+        "Molecular Biology",
+        "Museum Studies",
+        "Music",
+        "Natural Sciences",
+        "Neuroscience",
+        "Nursing",
+        "Nutrition Science",
+        "Pharmacy",
+        "Philosophy",
+        "Physics",
+        "Physics and Astronomy",
+        "Physics and Quantum Computing",
+        "Political Science",
+        "Politics and Philosophy",
+        "Psychology",
+        "Public and Professional Writing",
+        "Public Service",
+        "Rehabilitation Science",
+        "Religious Studies",
+        "Russian",
+        "Social Work",
+        "Sociology",
+        "Spanish",
+        "Statistics",
+        "Studio Arts",
+        "Supply Chain Management",
+        "Theatre Arts",
+        "Urban Planning and Geographic Analysis",
+        "Urban Studies"
+      ];
+
+  return (
+    <div className="container-fluid text-center">
+        <div className="row-2">
+            <h3> Please select your major: </h3>
+            <Dropdown>
+                <Dropdown.Toggle variant="secondary" id="dropdown-basic" 
+                style={{fontWeight: "bold", backgroundColor: "#003594", color: "#FFB81C"}}>
+                    Majors
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                    {majors.map((item) => (
+                        <Dropdown.Item href="#/action-1">{item}</Dropdown.Item>
+                    ))}
+                </Dropdown.Menu>
+            </Dropdown>
+        </div>
+    </div>
+  )
+}
+
+export default HomeScreen
---
+++ steelhacks2024/src/components/NavBar.tsx
@@ -0,0 +1,50 @@
+//import { Link } from "react-router-dom";
+import logo from "../assets/panthers_logo.png"
+
+function NavBar() {
+    return (
+        <nav className="navbar navbar-expand-lg" style={{backgroundColor: "#003594"}}>
+        <div className="container-fluid">
+            <div className="row-sm">
+                <a className="navbar-brand" href="" style={{fontSize: "24px", fontWeight: "bold", color: "#FFB81C"}}>
+                    <img src={logo} alt="Panther" width="30" height="24"/>
+                    <span>&nbsp; Pitt ClassFinder</span>
+                </a>
+            </div>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                {/*<li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li className="nav-item">
+                <a className="nav-link" href="#">Link</a>
+                </li>
+                <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Dropdown
+                </a>
+                <ul className="dropdown-menu">
+                    <li><a className="dropdown-item" href="#">Action</a></li>
+                    <li><a className="dropdown-item" href="#">Another action</a></li>
+                    <li><hr className="dropdown-divider" /></li>
+                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+                </li>
+                <li className="nav-item">
+                <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+                </li>*/}
+            </ul>
+            <form className="d-flex" role="search">
+                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                <button className="btn btn-outline-primary" type="submit" style={{fontWeight: "bold", color: "#FFB81C"}}>Search</button>
+            </form>
+            </div>
+        </div>
+        </nav>
+    );
+};
+
+export default NavBar;
