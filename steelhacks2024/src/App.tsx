851f04e4f500af23d5511c57a9ec871994b2041b
Zach Schultz <zachjacob7@gmail.com>
2024-09-28 21:14:22 -0400

    Response

--- steelhacks2024/src/App.tsx
+++ steelhacks2024/src/App.tsx
@@ -5,8 +5,28 @@ import HomeScreen from './components/HomeScreen';
 import NavBar from './components/NavBar';
 import Catalog from './components/Catalog';
 
+interface Response {
+  class1Name: string;
+  class1Number: string;
+  class2Name: string;
+  class2Number: string;
+  class3Name: string;
+  class3Number: string;
+  class4Name: string;
+  class4Number: string;
+}
+
 function App() {
-  const [response, setResponse] = useState('');
+  const [response, setResponse] = useState<Response>({
+    class1Name: "",     // E.g., Discete Structures
+    class1Number: "",   // E.g., CS 0441
+    class2Name: "",
+    class2Number: "",
+    class3Name: "",
+    class3Number: "",
+    class4Name: "",
+    class4Number: "",
+  });
 
   return (
     <>
@@ -14,7 +34,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="" element={<HomeScreen setResp={ setResponse }/>} />
-          <Route path="/classes" element={<Catalog resp={ response }/>} />
+          <Route path="/classes" element={<Catalog resp={ response } setResp={ setResponse }/>} />
         </Routes>
       </Router>
     </>
