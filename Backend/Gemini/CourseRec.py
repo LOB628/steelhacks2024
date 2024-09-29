35e37aa5eacc4f7345f67282c488deb25fcf0669
ColeMinersUnion <colehansen018@gmail.com>
2024-09-28 21:27:11 -0400

    hello

--- Backend/Gemini/CourseRec.py
+++ Backend/Gemini/CourseRec.py
@@ -12,8 +12,8 @@ def genRecommandtions(ScholarlyArea):
     genai.configure(api_key=key) 
     model = genai.GenerativeModel('gemini-1.5-flash')
 
-    prompt = "Below are a set of classes from the university of pittsburgh. What classes would you "\
-            " recommend to a student trying to learn more about " + ScholarlyArea + ". Please format your response in JSON format. "
+    prompt = "What classes would you recommend to a student trying"\
+            " to learn more about " + ScholarlyArea + ". Please format your response in JSON format. "
     print(prompt)
 
     #res = model.generate_content(prompt)
